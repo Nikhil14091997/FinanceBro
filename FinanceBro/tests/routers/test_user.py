f6bfeb7701f410e4c1413ff22d1f4ce5d19de1b0
Nikhil Kumar <nkumar82@sundevils.asu.edu>
2024-03-25 23:38:52 -0700

    Added mocking for DB, keeping it clean.

--- FinanceBro/tests/routers/test_user.py
+++ FinanceBro/tests/routers/test_user.py
@@ -1,11 +1,20 @@
+from tests.mock_dependencies import MockSession
+
 from fastapi.testclient import TestClient
 from main import app
 from datetime import datetime
 
+from unittest.mock import patch
+
+
 client = TestClient(app)
 
-def test_register_user_success():
-    # Test successful user registration
+@patch('dependencies.SessionLocal', return_value=MockSession())
+def test_register_user_success(mock_get_db):
+    mock_get_db.add.return_value = None  
+    mock_get_db.commit.return_value = None  
+    mock_get_db.refresh.return_value = None 
+
     response = client.post(
         "/user/register",
         json={
@@ -23,36 +32,3 @@ def test_register_user_success():
     assert response.json()["message"] == "User registered successfully"
     assert "data" in response.json()
     assert response.json()["data"]["email"] == "john.doe@example.com"
-
-def test_register_user_invalid_date_format():
-    response = client.post(
-        "/user/register",
-        json={
-            "first_name": "Jane",
-            "last_name": "Doe",
-            "email": "jane.doe@example.com",
-            "password": "secret",
-            "date_of_birth": "2000-01-01", 
-            "location_state": "Arizona",
-            "location_country": "USA"
-        }
-    )
-    assert response.status_code == 400
-    assert response.json()["detail"] == "Invalid date format. Use MM/DD/YYYY."
-
-# def test_register_user_duplicate_email():
-#     response = client.post(
-#         "/user/register",
-#         json={
-#             "first_name": "John",
-#             "last_name": "Doe",
-#             "email": "john.doe@example.com",  # Duplicate email
-#             "password": "secret",
-#             "date_of_birth": "01/01/2000",
-#             "location_state": "Arizona",
-#             "location_country": "USA"
-#         }
-#     )
-#     assert response.status_code == 400
-#     assert response.json()["detail"] == "A user with this email already exists."
-
